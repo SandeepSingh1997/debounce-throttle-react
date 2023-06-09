2792a0cdb8339e767718db40a737670650767b28
SandeepSingh1997 <sandeepsingh10101997@gmail.com>
2023-06-09 14:28:12 +0530

    added throttle

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,40 @@ import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [defaultCount, setDefaultCount] = useState(0);
+  const [debounceCount, setDebounceCount] = useState(0);
+  const [throttleCount, setThrottleCount] = useState(0);
+
   const defaultTextRef = useRef(null);
   const debounceTextRef = useRef(null);
   const throttleTextRef = useRef(null);
 
-  //DEBOUNCE CODE BELOW
+  // 1) DEBOUNCE CODE BELOW
+  /******************** 
+     Debounce: A technique to delay call to a callback for a certain
+     period of time AFTER THE INACTIVITY, so that after the delay the callback is executed 
+     with the latest state/change.
+     *********************/
 
   const updateDebounceText = debounce((text) => {
-    debounceTextRef.current.textContent = text;
+    setDebounceCount(debounceCount + 1);
+    debounceTextRef.current.textContent = ` (${debounceCount + 1}) : ` + text;
   });
 
   function debounce(callback, delay = 1000) {
-    //we declare this timeout variable to be able to hold the
-    //reference to the pervious timeout object so as to be able to clear it.
     let timeout;
     return (text) => {
-      clearTimeout(timeout);
+      if (timeout !== null) {
+        clearTimeout(timeout);
+      }
       timeout = setTimeout(() => {
         callback(text);
       }, delay);
     };
   }
 
-  /******ANOTHER WAY TO DO DEBOUNCE IF WE DON'T WANT TO USE CALLBACK
+  /******
+   2) DEBOUNCE IF WE DON'T WANT TO USE CALLBACK
    let timeout;
    
    function updateDebounceText(text, delay = 1000) {
@@ -34,14 +45,45 @@ function App() {
       }, delay);
     }*/
 
-  function handleInputChange(e) {
-    const currValue = e.target.value;
-    updateDefaultText(currValue);
-    updateDebounceText(currValue);
+  //THROTTLE CODE BELOW
+  /******************** 
+     Throttle: A technique similar to debounce but instead of executing the
+      callback after delay, starting from the time of inactivity, the callback
+      is executed in a specific interval, calling with the latest state/change.
+      *********************/
+
+  const updateThrottleText = throttle((text) => {
+    console.log("hello");
+    throttleTextRef.current.textContent = ` (${throttleCount + 1}) : ` + text;
+  });
+
+  function throttle(callback, delay = 1000) {
+    let currText = "";
+    let timeComplete = true;
+    return (text) => {
+      currText = text;
+      if (timeComplete) {
+        setTimeout(() => {
+          setThrottleCount(throttleCount + 1);
+          callback(currText);
+          timeComplete = true;
+        }, delay);
+        timeComplete = false;
+      }
+    };
   }
 
+  //DEFAULT
   function updateDefaultText(text) {
-    defaultTextRef.current.textContent = text;
+    setDefaultCount(defaultCount + 1);
+    defaultTextRef.current.textContent = ` (${defaultCount + 1}) : ` + text;
+  }
+
+  function handleInputChange(e) {
+    const currValue = e.target.value;
+    // updateDefaultText(currValue);
+    // updateDebounceText(currValue);
+    updateThrottleText(currValue);
   }
 
   return (
