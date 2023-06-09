b64f8a38cd1d5102c61e5b5325cff62947ecc3a5
SandeepSingh1997 <sandeepsingh10101997@gmail.com>
2023-06-09 11:55:59 +0530

    added logic for debounce

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,38 @@ function App() {
   const debounceTextRef = useRef(null);
   const throttleTextRef = useRef(null);
 
+  //DEBOUNCE CODE BELOW
+
+  const updateDebounceText = debounce((text) => {
+    debounceTextRef.current.textContent = text;
+  });
+
+  function debounce(callback, delay = 1000) {
+    //we declare this timeout variable to be able to hold the
+    //reference to the pervious timeout object so as to be able to clear it.
+    let timeout;
+    return (text) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        callback(text);
+      }, delay);
+    };
+  }
+
+  /******ANOTHER WAY TO DO DEBOUNCE IF WE DON'T WANT TO USE CALLBACK
+   let timeout;
+   
+   function updateDebounceText(text, delay = 1000) {
+     clearTimeout(timeout);
+     timeout = setTimeout(() => {
+       debounceTextRef.current.textContent = text;
+      }, delay);
+    }*/
+
   function handleInputChange(e) {
     const currValue = e.target.value;
     updateDefaultText(currValue);
+    updateDebounceText(currValue);
   }
 
   function updateDefaultText(text) {
